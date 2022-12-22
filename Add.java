c27b6076562bd6b0759633bbbbb6353d8b1252f4
AdithyaBhaskarKds <121079868+AdithyaBhaskarKds@users.noreply.github.com>
2022-12-21 20:37:02 -0600

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+package com.pages;
+
+public class Add {
+	public void doThis()
+	{
+		int n1=6;
+		int n2=4;
+		System.out.println("First Num: "+n1);
+		System.out.println("First Num: "+n2);
+		int n3=n1+n2;
+		System.out.println("Sum: "+n3);
+		System.out.println("Completed");
+	}
+
+}
